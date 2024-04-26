a2cc1b9c69901e0f8ce21b01008f8c3639e157cb
Ross <rossmorran@hotmail.co.uk>
2024-04-26 17:39:20 +0100

    Return recommendations from button clicks

--- recommender/urls.py
+++ recommender/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from recommender import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('recommendation/', views.get_recommendation, name='recommendation')
 ]
--- recommender/views.py
+++ recommender/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from recommender.models import Book
+import random
 
 def home(request):
     return render(request, 'recommender/home.html', {})
+
+def get_recommendation(request):
+
+    # cache query results?
+
+    # get a random recommendation
+    # what if the Books table is empty?
+    ids = [book.id for book in Book.objects.all()]
+    random_id = random.choice(ids)
+
+    recommendation = get_object_or_404(Book, pk=random_id)
+
+    return render(
+        request,
+        'recommender/recommendation.html', 
+        {'recommendation': recommendation}
+    )
